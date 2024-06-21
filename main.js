aa83496b2ddba0904ddc36f988c0d05619958f03
Lucas Dircksen Balcevicz <145569604+Lucas26022008@users.noreply.github.com>
2024-06-21 08:46:02 -0300

    s

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const barracas = document.querySelectorAll('.caixa');
 
 barracas.forEach((barraca) => {
   barraca.addEventListener('mouseover', () => {
+    barracas.forEach(b => b.classList.remove('active'));
+    barraca.classList.add('active');
     const info = barraca.querySelector('p');
     info.style.display = 'block';
   });
 
   barraca.addEventListener('mouseout', () => {
+    barraca.classList.remove('active');
     const info = barraca.querySelector('p');
     info.style.display = 'none';
   });
