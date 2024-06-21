0082e508d386311767afea88d1f8f5865073e2a5
Lucas Dircksen Balcevicz <145569604+Lucas26022008@users.noreply.github.com>
2024-06-21 08:51:22 -0300

    a

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const barracas = document.querySelectorAll('.caixa');
+const popup = document.querySelector('.popup');
+const popupIcon = document.getElementById('popup-icon');
+const popupTitle = document.getElementById('popup-title');
+const popupInfo = document.getElementById('popup-info');
 
 barracas.forEach((barraca) => {
   barraca.addEventListener('mouseover', () => {
-    barracas.forEach(b => b.classList.remove('active'));
-    barraca.classList.add('active');
-    const info = barraca.querySelector('p');
-    info.style.display = 'block';
+    const icon = barraca.getAttribute('data-icon');
+    const title = barraca.getAttribute('data-title');
+    const info = barraca.getAttribute('data-info');
+
+    popupIcon.src = icon;
+    popupTitle.textContent = title;
+    popupInfo.innerHTML = info.replace(/,/g, '<br>');
+
+    popup.style.display = 'flex';
+    setTimeout(() => {
+      popup.style.opacity = '1';
+    }, 10); // timeout to ensure transition effect
   });
 
   barraca.addEventListener('mouseout', () => {
-    barraca.classList.remove('active');
-    const info = barraca.querySelector('p');
-    info.style.display = 'none';
+    popup.style.opacity = '0';
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 300); // match this with the CSS transition duration
   });
 });
